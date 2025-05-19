eae4253731a51993402cbf9d0896367d121bfe6f
David Chung <davidchung@Davids-MacBook-Air.local>
2025-05-18 17:26:45 -0700

    set routes and css

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+
+export const Home = () => {
+    return <div className="min-h-screen ">
+        {/* Render Theme toggle */}
+
+        {/* Handle Background effects */}
+
+        {/* Navbar */}
+
+        {/* Main Content */}
+
+
+        {/* Footer */}
+        Home
+    </div>
+}
